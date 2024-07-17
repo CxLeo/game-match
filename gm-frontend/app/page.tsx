72b619a1a4dae1a350ffedd1846a774d816b4f2a
cxLeoo <leo@noqclinic.com>
2024-07-17 14:44:38 -0400

    set up frontend

---
+++ gm-frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" w-screen h-screen">
+      <p className="text-3xl text-center h-full w-full">Here</p>
+      
+    </main>
+  );
+}
